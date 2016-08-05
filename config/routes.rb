3359e87b44fdbaf0b04040b25e3d1752f3f00f8f
Heber Sandoval <he3ers@gmail.com>
2016-08-05 17:26:31 -0400

    Add a TextPost and ImagePost model that inherates from the Post model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :image_posts
+  resources :text_posts
   resources :posts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
