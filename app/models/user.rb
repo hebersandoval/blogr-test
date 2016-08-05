b79e052af85f4de09769d7797b09f7d29c79be6f
Heber Sandoval <he3ers@gmail.com>
2016-08-05 17:56:34 -0400

    Fix misspelled has_many association on User model to be 'image_posts'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
   has_many :text_posts, dependent: :destroy
-  has_many :image_post, dependent: :destroy
+  has_many :image_posts, dependent: :destroy
 
   def following?(leader)
     leaders.include? leader
