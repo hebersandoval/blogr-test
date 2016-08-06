7993799ad9b44e231aacaf5c8416e620d7012eca
Heber Sandoval <he3ers@gmail.com>
2016-08-06 08:13:39 -0400

    Add basic authentication to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ActiveRecord::Base
 
   has_many :comments
 
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+
   def following?(leader)
     leaders.include? leader
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :text_posts
   resources :posts
   resources :users
+
+  get 'signup', to: 'users#new', as: 'signup'
+
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
