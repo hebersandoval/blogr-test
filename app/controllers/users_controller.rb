e17d4db88a91766db9297c03615261f9b7f79806
Heber Sandoval <he3ers@gmail.com>
2016-08-08 20:14:04 -0400

    Add show page to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order("created_at DESC")
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,6 +16,8 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+
 
   private
 
